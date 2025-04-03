93042cd90f02a28921dd3d953e74f2f5d9f2c22e
Roman Gleba <romagleba@gmail.com>
2025-04-03 20:52:05 +0300

    add effect sidebar menuButton

--- src/Componets/home/Home.jsx
+++ src/Componets/home/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import styles from './Home.module.scss'
+import styles from './Home.module.scss';
 
 const Home = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src="/images/text.png" className={styles.foto} alt="опис зображення"/>
         </div>
     );
 };
 
 export default Home;
-
-
-
